cd1507d8280c23fe57b476dfaced4f6ccea80773
= <=>
2022-11-27 23:58:56 +0100

    separate oauth2 login to different module

---
+++ src/auth.rs
@@ -0,0 +1,61 @@
+use anyhow::Result;
+use oauth2::{AccessToken, PkceCodeVerifier, TokenResponse};
+
+use local_auth;
+
+fn create_oauth2_client(redirect_url: String) -> Result<oauth2::basic::BasicClient> {
+    Ok(
+        oauth2::basic::BasicClient::new(
+        oauth2::ClientId::new("lichess-desktop_NotAPenguin".to_string()),
+        None,
+        oauth2::AuthUrl::new("https://lichess.org/oauth".to_string())?,
+        Some(oauth2::TokenUrl::new("https://lichess.org/api/token".to_string())?)
+        )
+        .set_redirect_uri(oauth2::RedirectUrl::new(redirect_url)?)
+    )
+}
+
+/// generate an authorization URL. The user has to visit this URL and authorize access.
+fn generate_auth_url(client: &oauth2::basic::BasicClient, pkce: oauth2::PkceCodeChallenge, scopes: &[&str]) -> Result<oauth2::url::Url> {
+    let (auth_url, _) = client
+        .authorize_url(oauth2::CsrfToken::new_random)
+        .add_scopes(scopes.iter().map(|scope| oauth2::Scope::new(scope.to_string())))
+        .set_pkce_challenge(pkce)
+        .url();
+    Ok(auth_url)
+}
+
+async fn generate_auth_code(port: u16, url: &oauth2::url::Url) -> Result<String> {
+    // Create a temporary webserver that will listen to localhost:8080 for a request containing an authentication code
+    // from lichess.
+    let mut auth_server = local_auth::AuthListener::new(local_auth::port::Port::from(port)).await?;
+
+    // Now we redirect the user to the authorization page to complete the verification process, and start listening for
+    // requests on the webserver
+    // TODO: Make this open a browser dialog/be configurable by a parameter
+    println!("Browse to {:?} to authenticate", url.to_string());
+
+    auth_server.listen().await
+}
+
+async fn exchange_token(client: &oauth2::basic::BasicClient, pkce: PkceCodeVerifier, auth_code: String) -> Result<AccessToken> {
+    Ok(client
+        .exchange_code(oauth2::AuthorizationCode::new(auth_code))
+        .set_pkce_verifier(pkce)
+        .request_async(oauth2::reqwest::async_http_client).await?.access_token().clone())
+}
+
+/// Log in to lichess using OAuth2, and return an access token that can be used in the lichess API.
+pub async fn oauth2_login(scopes: &[&str]) -> Result<AccessToken> {
+    let port: u16 = 8080;
+    let redirect_url = "http://localhost:".to_owned() + &port.to_string() + "/";
+    let client = create_oauth2_client(redirect_url)?;
+
+    let (pkce_challenge, pkce_verifier) = oauth2::PkceCodeChallenge::new_random_sha256();
+    let auth_url = generate_auth_url(&client, pkce_challenge, &scopes)?;
+    let auth_code = generate_auth_code(port, &auth_url).await?;
+
+    // We can now trade our auth code for an access token.
+    // Note that these access tokens are long lived, and can probably be (safely) cached.
+    exchange_token(&client, pkce_verifier, auth_code).await
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+mod auth;
+
 use reqwest;
 use oauth2;
 use oauth2::reqwest::{async_http_client, Error, http_client};
@@ -16,67 +18,7 @@ pub struct Lichess {
 
 impl Lichess {
     pub async fn new() -> anyhow::Result<Lichess> {
-        let auth_port: u16 = 8080;
-        let auth_uri = "http://localhost:".to_owned() + &auth_port.to_string() + "/";
-        let client = oauth2::basic::BasicClient::new(
-            oauth2::ClientId::new("lichess-desktop_NotAPenguin".to_string()),
-            None, // no client secret?
-            oauth2::AuthUrl::new("https://lichess.org/oauth".to_string())?,
-            Some(oauth2::TokenUrl::new("https://lichess.org/api/token".to_string())?)
-        )
-
-        .set_redirect_uri(oauth2::RedirectUrl::new(auth_uri)?);
-
-        // generate a new PKCE challenge
-        let (pkce_challenge, verifier) = oauth2::PkceCodeChallenge::new_random_sha256();
-        // generate the full authorization URL
-        let (auth_url, csrf_token) = client
-            .authorize_url(oauth2::CsrfToken::new_random)
-            // Set desired scopes, we want read+write access for our client
-            .add_scope(oauth2::Scope::new("preference:read".to_string()))
-            // Set PKCE challenge
-            .set_pkce_challenge(pkce_challenge)
-            .url();
-
-        let mut auth_server = block_on(local_auth::AuthListener::new(local_auth::port::Port::from(auth_port)))?;
-        // User has to browse to this URL to complete the verification process
-        println!("Browse to {}", &auth_url);
-        let auth_code = block_on(auth_server.listen())?;
-        println!("Authentication code: {}", &auth_code);
-
-        // After that, we can trade it for an access token
-        let token_request = client
-            .exchange_code(oauth2::AuthorizationCode::new(auth_code.clone()))
-            .set_pkce_verifier(verifier);
-        let token_result = token_request.request_async(|req| {
-            //println!("Request URL: {:#?}", &req.url);
-            //println!("Request headers: {:#?}", &req.headers);
-            //println!("Request method: {:#?}", &req.method);
-            //println!("Request body: {:#?}", String::from_utf8(req.body.clone()).unwrap());
-            async_http_client(req)
-        }).await;
-        if let Err(e) = &token_result {
-            match e {
-                RequestTokenError::ServerResponse(response) => {
-                    println!("Server returned error response: {:?}", response.to_string());
-                }
-                RequestTokenError::Request(req) => {
-                    println!("Request error: {:?}", req);
-                }
-                RequestTokenError::Parse(err, bytes) => {
-                    println!("Parse error: {:?}\n[{:?}]", err, String::from_utf8(bytes.clone()).unwrap())
-                }
-                RequestTokenError::Other(err) => {
-                    println!("Other error: {:?}", err);
-                }
-            }
-        }
-
-        // REMOVE REMOVE REMOVE
-        if let Some(token) = token_result.ok() {
-            println!("Authentication successful: Token: {:?}", token.access_token().secret());
-        }
-
+        let token = auth::oauth2_login(vec!["preference:read"].as_slice()).await?;
         let http = reqwest::Client::new();
         Ok(Lichess {
             client: http
